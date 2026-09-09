c79b0b36a2fcdaba360361e41e5b2e594ccf3b43
Shorya <shoryajain2000@gmail.com>
2026-09-09 12:20:33 +0530

    9-9-26 2

--- reactdom/src/components/ICard.jsx
+++ reactdom/src/components/ICard.jsx
@@ -2,18 +2,18 @@
 import React from 'react' 
 import pic from '../images/156.png'
 
-function ICard(props) {
+function ICard(data) {
   return (
     <div>
-      <h2>College:{props.data.college}</h2>
+      <h2>College:{data.college}</h2>
       <div>
         <img src={pic} height={200} width={200} />
       </div>
-      <h2>Roll No.:{props.rollNo}</h2>
-      <h2>Name:{props.name}</h2>
-      <h2>Branch:{props.branch}</h2>
-      <h2>Section:{props.section}</h2>
-      <h2>Skills:{props.skills}</h2>
+      <h2>Roll No.:{data.rollNo}</h2>
+      <h2>Name:{data.name}</h2>
+      <h2>Branch:{data.branch}</h2>
+      <h2>Section:{data.section}</h2>
+      <h2>Skills:{data.skills}</h2>
     </div>
   )
 }
